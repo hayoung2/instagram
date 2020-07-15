f4256cd3217c0394e3f60067660d7ce1c919f350
hayoung2 <gkdud1308@naver.com>
2020-07-16 03:52:21 +0900

    Update project

---
+++ app/src/main/java/with/developer/myapplication/PostActivity.java
@@ -0,0 +1,119 @@
+package with.developer.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+public class PostActivity extends AppCompatActivity {
+
+    Uri imageUri;
+    String myUrl="";
+    StorageTask uploadTask;
+    StorageReference storageReference;
+
+    ImageView close,image_added;
+    TextView post;
+    EditText description;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        close=findViewById(R.id.close);
+        image_added=findViewById(R.id.image_added);
+        post=findViewById(R.id.post);
+        description=findViewById(R.id.description);
+        storageReference= FirebaseStorage.getInstance().getReference("posts");
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PostActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+
+        CropImage.activity().setAspectRatio(1,1)
+                .start(PostActivity.this);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver =getContentResolver();
+        MimeTypeMap mime=MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage(){
+        ProgressDialog progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Posting");
+        progressDialog.show();
+
+        if (imageUri !=null){
+            final StorageReference filereference =storageReference.child(System.currentTimeMillis()
+                    +"."+getFileExtension(imageUri));
+
+            uploadTask=filereference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if(!task.isComplete()){
+                        throw task.getException();
+                    }
+
+                    return filereference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){}
+                }
+            });
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
+        && resultCode==RESULT_OK){
+            CropImage.ActivityResult result=CropImage.getActivityResult(data);
+            imageUri=result.getUri();
+        }else {
+            Toast.makeText(this,"Somthing gone wrong"
+            , Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(PostActivity.this,MainActivity.class));
+        }
+    }
+}
